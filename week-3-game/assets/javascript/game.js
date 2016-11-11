0e2eca9e470be9e6210c19d94818af05d93e1143
Chris Parolini <chris.parolini@chatid.com>
2016-11-11 16:57:58 -0600

    all html and css needs for the game

--- week-3-game/assets/javascript/game.js
+++ week-3-game/assets/javascript/game.js
@@ -1,3 +1,9 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+window.onload = function () {
 
-var computerGuess = alphabet[Math.floor(Math.random()*26)];
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  var teams = ['Cowboys', 'Giants', 'Eagles', 'Redskins', 'Bears', 'Lions', 'Packers', 'Vikings', 'Falcons', 'Panthers', 'Saints', 'Buccaneers', 'Cardinals', 'Rams', '49ers', 'Seahawks', 'Bills', 'Dolphins', 'Patriots', 'Jets', 'Ravens', 'Bengals', 'Browns', 'Steelers', 'Texans', 'Colts', 'Jaguars', 'Titans', 'Broncos', 'Chiefs', 'Raiders', 'Chargers']
+
+  var team = teams[Math.floor(Math.random() * teams.length)];
